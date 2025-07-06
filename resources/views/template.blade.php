b6e04368edbefd1a452cb39e8ac2930f48817872
Black Stallion <black-stallion@localhost.localdomain>
2025-07-06 14:45:10 +0100

    Mon second commit

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -206,7 +206,7 @@
                         </li>
                         <li>
                             <a href="{{ route('devis.index') }}"
-                                class="block bg-blue-700 text-sm text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                class="block p-5 bg-blue-700 text-sm text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                                 Obtenir un dévis
                             </a>
                         </li>
